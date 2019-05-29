5adcf9d69cff77250caedcdfcf242fae1152fd55
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-05-28 21:59:43 -0600

    Agregar todo

---
+++ Proyecto/conocimiento.pl
@@ -0,0 +1,64 @@
+conocimiento('javascript',['Vas a manejar el modo fullstack?','Quieres manejar paginas de forma dinamica?','Deseas que contenga efectos visuales?','Te gustaria que fuera multiplataforma?','Vas a manejar formularios?','Necesitas usar funciones inmediatas?']).
+conocimiento('kotlin',['Vas a desarrollar para Android?','Se te dificulta interpretar el codigo?','Es un proyecto personal?','Tu aplicacion sera multiplataforma?','Te gustaria que manejara excepciones automaticamente?','Te molesta trabajar con clases?']).
+conocimiento('java',['Quieres desarrollar una app movil?','Quieres que se ejecute en tiempo real?','Quieres un lenguaje intuitivo?','Necesitas facilidad para hacer debug en las apps?','Quieres realizar testing?','Necesitas un emulador para probar las apps?']).
+conocimiento('php',['Utilizaras una base de datos en un servidor?','Quieres usar un lenguaje libre y abierto?','Quieres un lenguaje multiplataforma?','Usaras la base de datos en una aplicacion web?','Deseas tener multiples fuentes de soporte?','Quieres que sea estable, flexible y veloz?']).
+conocimiento('c_sharp',['Va a crear un videojuego?','Va a desarrollar un software orientado a objetos?','Va a desarrollar una interfaz grafica dinamica?','Va a hacer uso de SQL Server o Azure?','Va a crear un software con hilos?','Desea crear un software MVC (Modelo Vista Controlador)?']).
+conocimiento('ruby',['Quieres crear una pagina WEB?','Deseas usar codigo libre?','Quieres trabajar a nivel de carpetas?','Te gustaria desarrollar demos con facilidad?','Quieres que sea expandible?','Quieres usar pocas lineas de codigo?']).
+conocimiento('c_plus',['Apenas son tus inicios como programador?','Es un programa sencillo?','Quieres trabajar con hardware?','Desarrollas en linux o mac?','Quieres usar software libre?','Quieres que sea multiplataforma?']).
+conocimiento('swift',['Quieres desarrollar para IOS?','Tienes una computadora con mac OS?','Te gusta usar frameworks?','Necesitas controlar el flujo?','Tu app ocupa mucha seguridad?','Quieres un lenguaje facil?']).
+
+id_imagen_preg('Quieres desarrollar para IOS?','pregunta').
+id_imagen_preg('Tienes una computadora con mac OS?','pregunta').
+id_imagen_preg('Te gusta usar frameworks?','pregunta').
+id_imagen_preg('Necesitas controlar el flujo?','pregunta').
+id_imagen_preg('Tu app ocupa mucha seguridad?','pregunta').
+id_imagen_preg('Quieres un lenguaje facil?','pregunta').
+
+id_imagen_preg('Apenas son tus inicios como programador?','pregunta').
+id_imagen_preg('Es un programa sencillo?','pregunta').
+id_imagen_preg('Quieres trabajar con hardware?','pregunta').
+id_imagen_preg('Desarrollas en linux o mac?','pregunta').
+id_imagen_preg('Quieres usar software libre?','pregunta').
+id_imagen_preg('Quieres que sea multiplataforma?','pregunta').
+
+id_imagen_preg('Quieres crear una pagina WEB?','pregunta').
+id_imagen_preg('Deseas usar codigo libre?','pregunta').
+id_imagen_preg('Quieres trabajar a nivel de carpetas?','pregunta').
+id_imagen_preg('Te gustaria desarrollar demos con facilidad?','pregunta').
+id_imagen_preg('Quieres que sea expandible?','pregunta').
+id_imagen_preg('Quieres usar pocas lineas de codigo?','pregunta').
+
+id_imagen_preg('Va a crear un videojuego?','pregunta').
+id_imagen_preg('Va a desarrollar un software orientado a objetos?','pregunta').
+id_imagen_preg('Va a desarrollar una interfaz grafica dinamica?','pregunta').
+id_imagen_preg('Va a hacer uso de SQL Server o Azure?','pregunta').
+id_imagen_preg('Va a crear un software con hilos?','pregunta').
+id_imagen_preg('Desea crear un software MVC (Modelo Vista Controlador)?','pregunta').
+
+id_imagen_preg('Utilizaras una base de datos en un servidor?','pregunta').
+id_imagen_preg('Quieres usar un lenguaje libre y abierto?','pregunta').
+id_imagen_preg('Quieres un lenguaje multiplataforma?','pregunta').
+id_imagen_preg('Usaras la base de datos en una aplicacion web?','pregunta').
+id_imagen_preg('Deseas tener multiples fuentes de soporte?','pregunta').
+id_imagen_preg('Quieres que sea estable, flexible y veloz?','pregunta').
+
+id_imagen_preg('Vas a manejar el modo fullstack?','pregunta').
+id_imagen_preg('Quieres manejar paginas de forma dinamica?','pregunta').
+id_imagen_preg('Deseas que contenga efectos visuales?','pregunta').
+id_imagen_preg('Te gustaria que fuera multiplataforma?','pregunta').
+id_imagen_preg('Vas a manejar formularios?','pregunta').
+id_imagen_preg('Necesitas usar funciones inmediatas?','pregunta').
+
+id_imagen_preg('Quieres desarrollar una app movil?','pregunta').
+id_imagen_preg('Quieres que se ejecute en tiempo real?','pregunta').
+id_imagen_preg('Quieres un lenguaje intuitivo?','pregunta').
+id_imagen_preg('Necesitas facilidad para hacer debug en las apps?','pregunta').
+id_imagen_preg('Quieres realizar testing?','pregunta').
+id_imagen_preg('Necesitas un emulador para probar las apps?','pregunta').
+
+id_imagen_preg('Vas a desarrollar para Android?','pregunta').
+id_imagen_preg('Se te dificulta interpretar el codigo?','pregunta').
+id_imagen_preg('Es un proyecto personal?','pregunta').
+id_imagen_preg('Tu aplicacion sera multiplataforma?','pregunta').
+id_imagen_preg('Te gustaria que manejara excepciones automaticamente?','pregunta').
+id_imagen_preg('Te molesta trabajar con clases?','pregunta').
---
+++ Proyecto/main.pl
@@ -0,0 +1,132 @@
+ :- use_module(library(pce)).
+ :- pce_image_directory('./img').
+ :- use_module(library(pce_style_item)).
+ :-consult('./conocimiento.pl').
+ :-consult('./motorDeInferencia.pl').
+ :- dynamic color/2.
+
+resource(img_principal, image, image('fondo2.jpg')).
+resource(portada, image, image('fondo2.jpg')).
+resource(pregunta,image,image('pregunta.jpg')).
+resource(javascript,image,image('javascript.jpg')).
+resource(c_sharp,image,image('c_sharp.jpg')).
+resource(c,image,image('c.jpg')).
+resource(c_plus,image,image('c++.jpg')).
+resource(java,image,image('java.jpg')).
+resource(kotlin,image,image('kotlin.jpg')).
+resource(objetive,image,image('objetive.jpg')).
+resource(php,image,image('php.jpg')).
+resource(swift,image,image('swift.jpg')).
+resource(ruby,image,image('ruby.jpg')).
+
+ mostrar_imagen(Pantalla, Imagen) :- new(Figura, figure),
+                                     new(Bitmap, bitmap(resource(Imagen),@on)),
+                                     send(Bitmap, name, 1),
+                                     send(Figura, display, Bitmap),
+                                     send(Figura, status, 1),
+                                     send(Pantalla, display,Figura,point(100,80)).
+  mostrar_imagen_tratamiento(Pantalla, Imagen) :-new(Figura, figure),
+                                     new(Bitmap, bitmap(resource(Imagen),@on)),
+                                     send(Bitmap, name, 1),
+                                     send(Figura, display, Bitmap),
+                                     send(Figura, status, 1),
+                                     send(Pantalla, display,Figura,point(20,100)).
+ nueva_imagen(Ventana, Imagen) :-new(Figura, figure),
+                                new(Bitmap, bitmap(resource(Imagen),@on)),
+                                send(Bitmap, name, 1),
+                                send(Figura, display, Bitmap),
+                                send(Figura, status, 1),
+                                send(Ventana, display,Figura,point(0,0)).
+  imagen_pregunta(Ventana, Imagen) :-new(Figura, figure),
+                                new(Bitmap, bitmap(resource(Imagen),@on)),
+                                send(Bitmap, name, 1),
+                                send(Figura, display, Bitmap),
+                                send(Figura, status, 1),
+                                send(Ventana, display,Figura,point(500,60)).
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+  botones:-borrado,
+                send(@boton, free),
+                send(@btntratamiento,free),
+                mostrar_diagnostico(Enfermedad),
+                send(@texto, selection('Tu lenguaje de programacion es:')),
+                send(@resp1, selection(Enfermedad)),
+                new(@boton, button('Iniciar consulta',
+                message(@prolog, botones)
+                )),
+
+                new(@btntratamiento,button('Detalles y Tratamiento',
+                message(@prolog, mostrar_tratamiento,Enfermedad)
+                )),
+                send(@main, display,@boton,point(20,450)),
+                send(@main, display,@btntratamiento,point(138,450)).
+
+
+
+  mostrar_tratamiento(X):-new(@tratam, dialog('Tratamiento')),
+                          send(@tratam, append, label(nombre, 'Explicacion: ')),
+                          send(@tratam, display,@lblExp1,point(70,51)),
+                          send(@tratam, display,@lblExp2,point(50,80)),
+                          tratamiento(X),
+                          send(@tratam, transient_for, @main),
+                          send(@tratam, open_centered).
+
+tratamiento(X):- send(@lblExp1,selection('Debes usar este lenguaje de programacion')),
+                 mostrar_imagen_tratamiento(@tratam,X).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+   preguntar(Preg,Resp):-new(Di,dialog('Colsultar Datos:')),
+                        new(L2,label(texto,'Responde las siguientes preguntas')),
+                        id_imagen_preg(Preg,Imagen),
+                        imagen_pregunta(Di,Imagen),
+                        new(La,label(prob,Preg)),
+                        new(B1,button(si,and(message(Di,return,si)))),
+                        new(B2,button(no,and(message(Di,return,no)))),
+                        send(Di, gap, size(25,25)),
+                        send(Di,append(L2)),
+                        send(Di,append(La)),
+                        send(Di,append(B1)),
+                        send(Di,append(B2)),
+                        send(Di,default_button,'si'),
+                        send(Di,open_centered),get(Di,confirm,Answer),
+                        free(Di),
+                        Resp=Answer.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  interfaz_principal:-new(@main,dialog('QUE LENGUAJE DE PROG ELEGIR',
+        size(1000,1000))),
+        new(@texto, label(nombre,'El Diagnostico a partir de los datos es:',font('times','roman',18))),
+        new(@resp1, label(nombre,'',font('times','roman',22))),
+        new(@lblExp1, label(nombre,'',font('times','roman',14))),
+        new(@lblExp2, label(nombre,'',font('times','roman',14))),
+        new(@salir,button('SALIR',and(message(@main,destroy),message(@main,free)))),
+        new(@boton, button('Iniciar consulta',message(@prolog, botones))),
+
+        new(@btntratamiento,button('�Libro?')),
+
+        nueva_imagen(@main, img_principal),
+        send(@main, display,@boton,point(138,450)),
+        send(@main, display,@texto,point(20,130)),
+        send(@main, display,@salir,point(300,450)),
+        send(@main, display,@resp1,point(20,180)),
+        send(@main,open_centered).
+
+       borrado:- send(@resp1, selection('')).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  main:- new(@interfaz,dialog('Bienvenido al Sistema Experto Recomendador',
+  size(1000,1000))),
+
+  mostrar_imagen(@interfaz, portada),
+
+  new(BotonComenzar,button('COMENZAR',and(message(@prolog,interfaz_principal) ,
+  and(message(@interfaz,destroy),message(@interfaz,free)) ))),
+  new(BotonSalir,button('SALIDA',and(message(@interfaz,destroy),message(@interfaz,free)))),
+  send(@interfaz,append(BotonComenzar)),
+  send(@interfaz,append(BotonSalir)),
+  send(@interfaz,open_centered).
+
+  :-main.
